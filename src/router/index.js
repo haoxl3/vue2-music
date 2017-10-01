cf9f7159a6c86d55a4eed0f0e4b775a274e2916b
hao <hao>
2017-10-01 13:01:21 +0800

    '异步加载组件'

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,56 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
-import Singer from 'components/singer/singer'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
-import TopList from 'components/top-list/top-list'
-import UserCenter from 'components/user-center/user-center'
 
 Vue.use(Router)
+
+// 动态加载模块
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const Singer = (resolve) => {
+  import('components/singer/singer').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const Rank = (resolve) => {
+  import('components/rank/rank').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const Search = (resolve) => {
+  import('components/search/search').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const SingerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const Disc = (resolve) => {
+  import('components/disc/disc').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const TopList = (resolve) => {
+  import('components/top-list/top-list').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const UserCenter = (resolve) => {
+  import('components/user-center/user-center').then((recommend) => {
+    resolve(recommend)
+  })
+}
 
 export default new Router({
   routes: [
